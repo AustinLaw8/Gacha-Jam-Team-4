a3d57e7690b707244293b8689c65dcf0667d8322
daniels-magic <danieltshih@gmail.com>
2022-08-05 20:58:31 -0700

    basic enemy behavior, spawner, and score

--- Assets/Danie/Enemy1Behavior.cs
+++ Assets/Danie/Enemy1Behavior.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Enemy1Behavior : MonoBehaviour
 {
+
+    private float moveSpeed;
+    private float dirX;
+    private Rigidbody2D rb;
+    private Vector3 localScale;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        localScale = transform.localScale; //moves enemy
+        rb = GetComponent<Rigidbody2D>();
+        dirX = -1f;
+        moveSpeed = 2f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D data)
+    {
+        
+        if (data.CompareTag("Player"))
+        {
+            Destroy(data.gameObject);
+            GameLogic.inprogress = false;
+            Debug.Log("u suck");
+        }
+
+
     }
 }
---
+++ Assets/Danie/Enemy1Spawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Spawner : MonoBehaviour
+{
+
+    public GameObject Enemy1;
+
+    private float time = 0;
+    public float spawnTime = 1f;
+
+    public float maxLeft = -3f;
+    public float maxRight = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > spawnTime && GameLogic.inprogress) 
+        {
+            GameObject enemy = Instantiate(Enemy1);
+            enemy.transform.position = transform.position = new Vector2(Random.Range(maxLeft, maxRight), 0); //spawn in the right place
+            Destroy(Enemy1, 10); //destroy barrier after 10 seconds to prevent memory loss
+            time = 0;
+        }
+
+        time = time + Time.deltaTime;
+    }
+}
