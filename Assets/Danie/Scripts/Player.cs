862eb6a5b37b0f383f5ca9a03d825c2bda52e2fd
daniels-magic <danieltshih@gmail.com>
2022-08-06 14:45:41 -0700

    player start

---
+++ Assets/Danie/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float jumpAmount = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnFly(InputAction action)
+    {
+        rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
